00a9b084f291f4cf7eff7d30d311dfd65c063e1c
karnhao <karn0888422952@gmail.com>
2022-06-26 23:30:54 +0700

    setup dependencies

---
+++ dist/widget.js
@@ -0,0 +1,7 @@
+"use strict";
+class Main {
+    static main(args) {
+        console.log("Hello World");
+    }
+}
+Main.main();
---
+++ src/widget.ts
@@ -0,0 +1,7 @@
+class Main {
+    public static main(args?: String[]): void {
+        console.log("Hello World");
+    }
+}
+
+Main.main();
