d902eae372af382d5e382ab0ed7286c04a4f3de3
karnhao <karn0888422952@gmail.com>
2022-06-27 00:14:22 +0700

    6/27/2022 12:14 AM

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # haokuwidget
 Widget in iOS
 
+This widget can display a list of subject on your home screen depending on your current time.
+
+ - [Download Scriptable](https://scriptable.app/)
+
+
+# Status
 กำลังพัฒนา...
+
+เริ่มต้นพัฒนาในวันที่ 27 มิถุนายน 2565
+
+# Contributing
+ - ท่านสามารถสร้าง Issue, Pull Request ได้
+ - ท่านสามารถสร้าง Fork แยกออกมาได้
+
+ # Contract
+  - Discord : karnhao#5238
+  - eMail : karn0888422952@gmail.com
+  - Tel : 094-698-1168
--- src/widget.ts
+++ src/widget.ts
@@ -1,7 +1,28 @@
-class Main {
-    public static main(args?: String[]): void {
-        console.log("Hello World");
+class Subject {
+    private startTime: number = 0;
+    private width: number = 0;
+    private name_th: string = "";
+    private name_en: string = "";
+    private id: string = "";
+    private day: number | null = null;
+}
+
+class SubjectDay {
+    private subjectList: Subject[] = [];
+
+    public putSubject(subject: Subject): void {
+        this.subjectList.push(subject);
     }
 }
 
-Main.main();
+class Table {
+    private days = {
+        _0: new SubjectDay(),
+        _1: new SubjectDay(),
+        _2: new SubjectDay(),
+        _3: new SubjectDay(),
+        _4: new SubjectDay(),
+        _5: new SubjectDay(),
+        _6: new SubjectDay(),
+    }
+}
