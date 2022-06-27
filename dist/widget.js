f6e6ec98ae352fd6a6c972c07db9b01a2551ca7e
karnhao <karn0888422952@gmail.com>
2022-06-28 01:19:33 +0700

    6/28/2022 1:19 AM

--- dist/widget.js
+++ dist/widget.js
@@ -1,7 +1,102 @@
 "use strict";
-class Main {
-    static main(args) {
-        console.log("Hello World");
+const Setting = {
+    user: {
+        username: undefined,
+        password: undefined
+    },
+    isValid: function () {
+        return this.user.username != null && this.user.password != null;
+    }
+};
+class Subject {
+    startTime = 0;
+    width = 0;
+    name_th = "";
+    name_en = "";
+    id = "";
+    day = null;
+    getStartTime() {
+        return this.startTime;
+    }
+    setStartTime(value) {
+        this.startTime = value;
+    }
+    getEndTime() {
+        return this.startTime + this.width;
+    }
+    getWidth() {
+        return this.width;
+    }
+    setWidth(value) {
+        this.width = value;
+    }
+    getNameTH() {
+        return this.name_th;
+    }
+    setNameTH(value) {
+        this.name_th = value;
+    }
+    getNameEN() {
+        return this.name_en;
+    }
+    setNameEN(value) {
+        this.name_en = value;
+    }
+    getID() {
+        return this.id;
+    }
+    setID(value) {
+        this.id = value;
+    }
+    getDay() {
+        return this.day;
+    }
+    setDay(value) {
+        this.day = value;
     }
 }
-Main.main();
+class SubjectDay {
+    subjectList = [];
+    name;
+    name_th;
+    constructor(name_en, name_th) {
+        this.name = name_en;
+        this.name_th = name_th ?? this.name;
+    }
+    getDayNameEN() {
+        return this.name;
+    }
+    getDayNameTH() {
+        return this.name_th;
+    }
+    putSubject(subject) {
+        this.subjectList.push(subject);
+    }
+    getSubject(index) {
+        return this.subjectList[index];
+    }
+}
+class Table {
+    days = {
+        _0: new SubjectDay("Sunday", "อาทิตย์"),
+        _1: new SubjectDay("Monday", "จันทร์"),
+        _2: new SubjectDay("Tuesday", "อังคาร"),
+        _3: new SubjectDay("Wednesday", "พุธ"),
+        _4: new SubjectDay("Thursday", "พฤหัสบดี"),
+        _5: new SubjectDay("Friday", "ศุกร์"),
+        _6: new SubjectDay("Saturday", "เสาร์"),
+    };
+    static parse(data) {
+        //TODO : parse data
+        return new Table();
+    }
+}
+class User {
+    id;
+    name_th;
+    name_en;
+    static parse(data) {
+        //TODO : parse data
+        return new User();
+    }
+}
--- src/widget.ts
+++ src/widget.ts
@@ -1,3 +1,98 @@
+export interface Root {
+    code: string
+    message: string
+    accesstoken: string
+    renewtoken: string
+    user: User
+    cache: boolean
+}
+
+export interface User {
+    loginName: string
+    userType: string
+    idCode: string
+    titleTh: string
+    titleEn: string
+    firstNameTh: string
+    firstNameEn: string
+    middleNameTh: any
+    middleNameEn: any
+    lastNameTh: string
+    lastNameEn: string
+    avatar: string
+    gender: string
+    student: Student
+    roleMenus: RoleMenu[]
+}
+
+export interface Student {
+    loginName: string
+    stdId: string
+    stdCode: string
+    titleTh: string
+    titleEn: string
+    firstNameTh: string
+    middleNameTh: any
+    lastNameTh: string
+    firstNameEn: string
+    middleNameEn: any
+    lastNameEn: string
+    copenId: string
+    copenNameTh: string
+    copenNameEn: string
+    campusCode: string
+    campusNameTh: string
+    campusNameEn: string
+    facultyCode: string
+    facultyNameTh: string
+    facultyNameEn: string
+    departmentCode: string
+    departmentNameTh: string
+    departmentNameEn: string
+    majorCode: string
+    majorNameTh: string
+    majorNameEn: string
+    nationCode: string
+    nationalityNameTh: string
+    nationalityNameEn: string
+    studentStatusCode: string
+    studentStatusNameTh: string
+    studentStatusNameEn: string
+    studentTypeCode: string
+    studentTypeNameTh: string
+    studentTypeNameEn: string
+    edulevelCode: string
+    edulevelNameTh: string
+    edulevelNameEn: string
+    studentYear: string
+    advisorId: string
+    advisorNameTh: string
+    advisorNameEn: string
+    positionTh: string
+    email: string
+    mobileNo: string
+}
+
+export interface RoleMenu {
+    menuId: number
+    menuNameTh: string
+    menuUrl?: string
+    menuIcon?: string
+    parentMenuId: number
+    menuType: number
+}
+
+
+const Setting = {
+    user: {
+        username: undefined,
+        password: undefined
+    },
+    isValid: function () {
+        return this.user.username != null && this.user.password != null;
+    }
+}
+
 class Subject {
     private startTime: number = 0;
     private width: number = 0;
@@ -5,24 +100,100 @@ class Subject {
     private name_en: string = "";
     private id: string = "";
     private day: number | null = null;
+
+    public getStartTime(): number {
+        return this.startTime;
+    }
+
+    public setStartTime(value: number) {
+        this.startTime = value;
+    }
+
+    public getEndTime(): number {
+        return this.startTime + this.width;
+    }
+
+    public getWidth(): number {
+        return this.width;
+    }
+
+    public setWidth(value: number) {
+        this.width = value;
+    }
+
+    public getNameTH(): string {
+        return this.name_th;
+    }
+
+    public setNameTH(value: string) {
+        this.name_th = value;
+    }
+
+    public getNameEN(): string {
+        return this.name_en;
+    }
+
+    public setNameEN(value: string) {
+        this.name_en = value;
+    }
+
+    public getID(): string {
+        return this.id;
+    }
+
+    public setID(value: string) {
+        this.id = value;
+    }
+
+    public getDay(): number | null {
+        return this.day;
+    }
+
+    public setDay(value: number | null) {
+        this.day = value;
+    }
 }
 
 class SubjectDay {
     private subjectList: Subject[] = [];
+    private name: string | undefined;
+    private name_th: string | undefined;
+
+    constructor(name_en?: string, name_th?: string) {
+        this.name = name_en;
+        this.name_th = name_th ?? this.name;
+    }
+
+    public getDayNameEN(): string | undefined {
+        return this.name;
+    }
+
+    public getDayNameTH(): string | undefined {
+        return this.name_th;
+    }
 
     public putSubject(subject: Subject): void {
         this.subjectList.push(subject);
+    }
+
+    public getSubject(index: number): Subject {
+        return this.subjectList[index];
     }
 }
 
 class Table {
     private days = {
-        _0: new SubjectDay(),
-        _1: new SubjectDay(),
-        _2: new SubjectDay(),
-        _3: new SubjectDay(),
-        _4: new SubjectDay(),
-        _5: new SubjectDay(),
-        _6: new SubjectDay(),
+        _0: new SubjectDay("Sunday", "อาทิตย์"),
+        _1: new SubjectDay("Monday", "จันทร์"),
+        _2: new SubjectDay("Tuesday", "อังคาร"),
+        _3: new SubjectDay("Wednesday", "พุธ"),
+        _4: new SubjectDay("Thursday", "พฤหัสบดี"),
+        _5: new SubjectDay("Friday", "ศุกร์"),
+        _6: new SubjectDay("Saturday", "เสาร์"),
+    }
+
+    public static parse(data: any): Table {
+        //TODO : parse data
+        return new Table();
     }
 }
